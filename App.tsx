d7513134736b2727d35dfbdc3cff3fe6fd59f258
Wesley Guerra <wesley.guerra@acpgroup.com.br>
2022-11-20 20:57:49 -0300

    LogBix

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Fragment } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Fragment>
+      <Text key="1">React</Text>
+      <Text key="2">Native</Text>
+    </Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
